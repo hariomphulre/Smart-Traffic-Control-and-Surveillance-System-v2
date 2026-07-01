e777eb86ac940c3fc2a23498e566760688dc1f83
hariomphulre <hariiomphullre@gmail.com>
2026-07-02 00:57:47 +0530

    test model

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -1,17 +1,29 @@
 'use client'
+import Link from 'next/link';
 import React from 'react'
 import { CgMenuGridR } from "react-icons/cg";
+import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
+
 const Sidebar = () => {
   return (
-
     <div className="h-full w-12 border-r dark:border-[#3c4043] dark:bg-[#131314]">
       <div className="flex pr-1 h-[48.99px] border-b dark:border-[#3c4043] items-center justify-center">
           <CgMenuGridR className="h-8 w-8 text-gray-300"/>
       </div>
-      <div>
-
-      </div>
+      <div className="">
+        <nav>
+          <ul>
+            <li>
+              <HiOutlineClipboardDocumentList/>
+            </li>
+            <li>
+              <HiOutlineClipboardDocumentList/>
+            </li>
+          </ul>
+        </nav>
+      </div>      
     </div>
+    
   )
 }
 
