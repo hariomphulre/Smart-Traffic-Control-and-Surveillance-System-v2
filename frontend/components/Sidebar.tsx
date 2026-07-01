b88b3005b82f7b7b115fc32baa6865ecd87c0356
hariomphulre <hariiomphullre@gmail.com>
2026-07-02 04:05:26 +0530

    test model

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -5,25 +5,46 @@ import { CgMenuGridR } from "react-icons/cg";
 import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
 import { IoMdListBox } from 'react-icons/io';
 import { MdOutlineMonitor } from 'react-icons/md';
+import { GoPasskeyFill } from "react-icons/go";
+import { RiKeyFill, RiShieldKeyholeFill, RiShieldUserFill } from 'react-icons/ri';
+import { useRouter } from "next/navigation";
 
 const Sidebar = () => {
+  const router=useRouter();
+
+  const handleIAM=async ()=>{
+    router.push('/iam');
+  }
+  const handleSessions=async ()=>{
+    router.push('/sessions');
+    
+  }
+  const handleAuditLogs=async ()=>{
+    router.push('/audit-logs');
+
+  }
+
   return (
     <div className="h-full w-12 border-r dark:border-[#3c4043] dark:bg-[#131314]">
-      <div className="flex pr-1 h-[48.99px] border-b dark:border-[#3c4043] items-center justify-center">
+      <div className="flex pr-0.5 h-[48.99px] border-b dark:border-[#3c4043] items-center justify-center">
           <CgMenuGridR className="h-8 w-8 text-gray-300"/>
       </div>
-      <div className="">
-        <nav>
+      <div className="flex justify-center">
+        <nav className="pr-0.5">
           <ul>
             <li>
-              <HiOutlineClipboardDocumentList className="h-5 w-5"/>
+              <RiShieldUserFill onClick={handleIAM} className="mt-4 h-5 w-5" title="IAM"/>
             </li>
             <li>
-              <MdOutlineMonitor className="h-5 w-5"/>
+              <MdOutlineMonitor onClick={handleSessions} className="mt-4 h-5 w-5" title="Sessions"/>
             </li>
             <li>
-              <IoMdListBox className="h-5 w-5"/>
+              <IoMdListBox onClick={handleAuditLogs} className="mt-4 h-5 w-5" title="Audit Logs"/>
             </li>
+            <li>
+              <RiKeyFill className="mt-4 h-5 w-5" title="Passkeys"/>
+            </li>
+    
           </ul>
         </nav>
       </div>      
