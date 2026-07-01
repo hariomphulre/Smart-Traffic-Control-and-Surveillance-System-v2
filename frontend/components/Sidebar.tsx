1fcd8e74ccd80bdb85f0ad21d8af4d59ce3df5ac
hariomphulre <hariiomphullre@gmail.com>
2026-07-01 15:34:35 +0530

    add models to lfs

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -1,16 +1,24 @@
 'use client'
 import React from 'react'
 import { CgMenuGridR } from "react-icons/cg";
-
+import { useWindowSize } from '@/hooks/useWindowSize';
 const Sidebar = () => {
+  const {width} =useWindowSize();
   return (
-    <div className="h-full w-12 border-r dark:border-[#3c4043] dark:bg-[#131314]">
-        <div className="flex pr-1 h-[48.99px] border-b dark:border-[#3c4043] items-center justify-center">
-            <CgMenuGridR className="h-8 w-8 text-gray-300"/>
-        </div>
-        <div>
 
+    <div className="h-full w-12 border-r dark:border-[#3c4043] dark:bg-[#131314]">
+      {width>=1150 ?
+        <div>
+          <div className="flex pr-1 h-[48.99px] border-b dark:border-[#3c4043] items-center justify-center">
+              <CgMenuGridR className="h-8 w-8 text-gray-300"/>
+          </div>
+          <div>
+
+          </div>
         </div>
+        :
+        <div> Please use larger screen</div>
+      }
         
     </div>
   )
